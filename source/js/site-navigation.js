5274f8ce8b5b3256663cf4ddb4642920a16b3b45
munkimu <naduh@list.ru>
2022-12-11 18:39:25 +0300

    Адаптивная сетка

--- source/js/site-navigation.js
+++ source/js/site-navigation.js
@@ -1,4 +1,11 @@
 document.querySelector('.site-navigation__toggle').onclick = function() {
   document.querySelector('.site-navigation__toggle').classList.toggle('site-navigation__toggle--is-open');
-  document.querySelector('.site-navigation-list').classList.toggle('visually-hidden');
+  document.querySelector('.site-navigation-list').classList.toggle('site-navigation-list--visible');
+  if(document.querySelector('.site-navigation-list').classList.contains('site-navigation-list--visible')) {
+    document.querySelector('.site-navigation-list').classList.remove('site-navigation-list--not-visible');
+  }
+  else {
+    document.querySelector('.site-navigation-list').classList.add('site-navigation-list--not-visible');
+  }
+
 }
