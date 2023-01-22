68c8cea329e9079d7b97a5050ac41b2f1ff9d4d9
munkimu <naduh@list.ru>
2023-01-22 17:50:49 +0300

    Защита-2

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,18 @@
-let navToggle = document.querySelector('.site-navigation__toggle');
+let navToggleWrapper = document.querySelector('.site-navigation__toggle-wrapper');
 let navList = document.querySelector('.site-navigation-list');
+let Logo = document.querySelector('.header__logo');
+let navListWrapper = document.querySelector('.site-navigation__list-wrapper');
+let navToggle = document.querySelector('.site-navigation__toggle');
+
+
 let mapFrame = document.querySelector('.map__frame');
 let mapImage = document.querySelector('.map__image');
 
 navList.classList.remove('site-navigation-list--no-js');
-navToggle.classList.remove('site-navigation__toggle--no-js');
+navToggleWrapper.classList.remove('site-navigation__toggle-wrapper--no-js');
+Logo.classList.remove('header__logo--no-js');
+navListWrapper.classList.remove('site-navigation__list-wrapper--no-js');
+
 navList.classList.add('site-navigation-list--not-visible');
 
 navToggle.onclick = function() {
